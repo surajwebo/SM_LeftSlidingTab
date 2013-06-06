3ca8caa684a0cc3c3539aaaa03af30ff8b541b61
surajwebo <jay@Dhananjays-MacBook-Pro.local>
2013-06-06 16:30:39 +0530

    Path & Facebook like Left Sliding Menu.

---
+++ LeftSlidingTabDemo/TabViewController.h
@@ -0,0 +1,11 @@
+//
+//  FirstViewController.h
+//  SampleTabController
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ LeftSlidingTabDemo/TabViewController.m
@@ -0,0 +1,41 @@
+//
+//  FirstViewController.m
+//  SampleTabController
+//
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+- (IBAction)goBack:(id)sender;
+@end
+
+@implementation FirstViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)goBack:(id)sender
+{
+    [self.tabBarController dismissViewControllerAnimated:NO completion:NULL];
+}
+
+@end
