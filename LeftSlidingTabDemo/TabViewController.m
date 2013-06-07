9925cf6f76357735da57da2a26bc6dd1c09e3279
surajwebo <jay@Dhananjays-MacBook-Pro.local>
2013-06-07 12:27:02 +0530

    YabViewController: UI changes.

--- LeftSlidingTabDemo/TabViewController.m
+++ LeftSlidingTabDemo/TabViewController.m
@@ -5,9 +5,10 @@
 //
 
 #import "TabViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface TabViewController ()
-- (IBAction)goBack:(id)sender;
+
 @end
 
 @implementation TabViewController
@@ -24,7 +25,10 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    [self enhanceUI :tabBtn1 :[UIColor lightGrayColor] :[UIColor blackColor]];
+    [self enhanceUI :tabBtn2 :[UIColor lightGrayColor] :[UIColor yellowColor]];
+    [self enhanceUI :tabBtn3 :[UIColor lightGrayColor] :[UIColor orangeColor]];
+    [self enhanceUI :tabBtn4 :[UIColor lightGrayColor] :[UIColor brownColor]];
 }
 
 - (void)didReceiveMemoryWarning
@@ -33,10 +37,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)goBack:(id)sender
-{
-    [self.tabBarController dismissViewControllerAnimated:NO completion:NULL];
-}
 
 - (IBAction)tabBtn1Action:(id)sender {
     [[[UIAlertView alloc] initWithTitle:@"Alert" message:@"Tab 1 pressed" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil] show];
@@ -66,7 +66,7 @@
     [UIView setAnimationDuration:0.75];
     [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
     [viewForTabs setFrame:CGRectMake(self.view.bounds.origin.x, viewForTabs.bounds.origin.y,viewForTabs.bounds.size.width,viewForTabs.bounds.size.height)];
-    [tabSliderBtn setTitle:@"<<" forState:UIControlStateNormal];
+    [tabSliderBtn setTitle:@"<[" forState:UIControlStateNormal];
     [UIView commitAnimations];
     isTabViewSlided = YES;
     
@@ -78,9 +78,29 @@
     [UIView setAnimationDuration:0.75];
     [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
     [viewForTabs setFrame:CGRectMake(viewForTabs.bounds.origin.x- 215, viewForTabs.bounds.origin.y,viewForTabs.bounds.size.width,viewForTabs.bounds.size.height)];
-    [tabSliderBtn setTitle:@">>" forState:UIControlStateNormal];
+    [tabSliderBtn setTitle:@"]>" forState:UIControlStateNormal];
     [UIView commitAnimations];
     isTabViewSlided = NO;
+}
+
+-(void)enhanceUI:(UIButton *)button :(UIColor *)color1 :(UIColor *)color2 {
+    CAGradientLayer *layer1 = [CAGradientLayer layer];
+    NSArray *colors = [NSArray arrayWithObjects:
+                       (id)color1.CGColor,
+                       (id)color2.CGColor,
+                       nil];
+    [layer1 setColors:colors];
+    [layer1 setFrame:button.bounds];
+    [button.layer insertSublayer:layer1 atIndex:0];
+    button.clipsToBounds = YES;
     
+    [button setTitleColor:[UIColor greenColor] forState:UIControlStateHighlighted];
+    
+    [button.layer setBorderColor:[UIColor blackColor].CGColor];
+    [button.layer setBorderWidth:2.0f];
+    [button.layer setShadowColor:[UIColor whiteColor].CGColor];
+    [button.layer setShadowOpacity:0.8];
+    [button.layer setShadowOffset:CGSizeMake(2.0, 2.0)];
+    [button.layer setCornerRadius:5.0];
 }
 @end
